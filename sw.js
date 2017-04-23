713152d8216b721e8ff9353f4b2d05fcfe383973
Pmc Dev <phoomparin@gmail.com>
2017-04-23 23:47:16 +0700

    ⚡️ PageSpeed Optimizations

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ toolbox.precache([
   "css/main.css",
   "img/flask.blue.svg", "img/me.min.jpg",
   "js/pjax.js", "js/main.js", "js/sw-toolbox.js", "js/sw-toolbox-cache.js",
-  "https://fonts.googleapis.com/css?family=Roboto:300,400,500",
+  "https://fonts.googleapis.com/css?family=Roboto:300,400",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://code.jquery.com/jquery-3.2.1.min.js",
   "https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.1/js/materialize.min.js"
@@ -35,8 +35,8 @@ toolbox.router.get("/(.*)", function get(req, vals, opts) {
 toolbox.router.get("/css/*.css", toolbox.fastest)
 toolbox.router.get("/js/*.js", toolbox.fastest)
 toolbox.router.get("/fonts/*", toolbox.fastest)
-toolbox.router.get("/img/.jpg", toolbox.fastest)
-toolbox.router.get("/img/.webp", toolbox.fastest)
+toolbox.router.get("/img/*.jpg", toolbox.fastest)
+toolbox.router.get("/img/*.webp", toolbox.fastest)
 toolbox.router.get("/img/*.svg", toolbox.fastest)
 toolbox.router.get("/img/*.png", toolbox.fastest)
 toolbox.router.get("/browserconfig.xml", toolbox.fastest)
