afebb7e42fe8553c5bb1c384316e83d9ceceae47
Pmc Dev <phoomparin@gmail.com>
2017-05-05 14:31:49 +0700

    Minor Styling Improvements

--- js/main.js
+++ js/main.js
@@ -17,6 +17,19 @@ function init() {
   $(".wip").click(function() {
     $(this).fadeOut()
   })
+
+  $("select").material_select()
+
+  $("form.contact-form").submit(function(e) {
+    e.preventDefault()
+    var data = $(this).serializeArray().reduce(function(m,o) {
+      m[o.name] = o.value
+      return m
+    }, {})
+    Materialize.toast("Thanks for contacting me! I'll be in touch via e-mail very soon.", 1500)
+    console.log("Data is", data)
+    $(this).trigger("reset")
+  })
 }
 
 $("document").ready(function() {
